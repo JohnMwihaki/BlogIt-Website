76f7430bc036eb3dc4edab10e5e76ec6aa96730a
John Mwihaki <jmwkimani@gmail.com>
2025-07-15 12:50:03 +0300

    intergrating client to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ import uploadRoute from './routes/upload.routes'
 dotenv.config()
 export const app= express();
 app.use(cors({
-    origin:'https://blog-it-website-92p6.vercel.app/',
+    origin:'https://blog-it-website-92p6.vercel.app',
     credentials:true,
 }))
 app.use(express.json())
